9dfaefcf3668243ff7305b26cff43dc9727eadf3
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-05-25 15:43:58 +0300

    25.05.2021

---
+++ NorthwindBackend/Business/DependencyResolvers/AutoFac/AutoFacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.AutoFac
+{
+    public class AutoFacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
