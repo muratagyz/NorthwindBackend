89e95c745d5fe56c9ed0cbd5fef5df3c5c8c6442
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-08-03 13:34:38 +0300

    3.08.2021

--- NorthwindBackend/WebAPI/Startup.cs
+++ NorthwindBackend/WebAPI/Startup.cs
@@ -1,3 +1,6 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encyption;
 using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -51,7 +54,12 @@ namespace WebAPI
                     IssuerSigningKey= SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                 };
             });
+
+            services.AddDependencyResolvers(new ICoreModule[] {
+                new CoreModule()
+            }); ;
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
